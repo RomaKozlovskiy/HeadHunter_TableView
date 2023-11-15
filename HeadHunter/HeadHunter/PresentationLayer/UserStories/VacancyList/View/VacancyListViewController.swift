56fc01e700778f0b5129d483b417c8b1402e57a1
RomanKozlovskiy <roma.kozlovskiy.96@mail.ru>
2023-11-15 17:03:11 +0300

    basic project settings

---
+++ HeadHunter/HeadHunter/PresentationLayer/UserStories/VacancyList/View/VacancyListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  VacancyListViewController.swift
+//  HeadHunter
+//
+//  Created by Роман Козловский on 15.11.2023.
+//
+
+import UIKit
+
+class VacancyListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemTeal
+    }
+}
+
